b35d8bd1337d5fe15ce013291d052e56eb7f1083
Sunday Adefila <adefilaedward@gmail.com>
2016-07-28 13:24:29 +0100

    change validation specs from using all attributes to using validators

--- lib/generators/model_spec/templates/model_spec.rb
+++ lib/generators/model_spec/templates/model_spec.rb
@@ -1,43 +1,44 @@
 <% klass = class_name.camelize.singularize.constantize %>
 
-<% attributes = klass.new.attributes.except('id', "reset_password_token", "reset_password_sent_at", "remember_created_at", "sign_in_count", "current_sign_in_at", "last_sign_in_at", "current_sign_in_ip", "last_sign_in_ip", "created_at", "updated_at").keys %>
-# maybe use validators for this? (validators = (eval class_name).validators)
+<% validations = klass.validators.group_by{|a| a.kind} %>
+<% validators = validations.keys %>
 
 <% owner_klasses = klass.reflect_on_all_associations(:belongs_to).map{|asc| asc.name.to_s} %>
 <% supa_klasses = klass.reflections.collect{|a, b| b.class_name.underscore if b.macro==:has_many}.compact %>
 <% direct_supa_klasses = klass.reflections.collect{|a, b| b.class_name.underscore if b.macro==:has_one}.compact %>
 # TODO: maybe add one for :has_and_belongs_to_many?
-# TODO: ASSOCIATIONS, deletions, etc...
 # TODO: All tests for has through are failing miserably
-
-<% if attributes.include? 'encrypted_password' %>
- <% attributes[attributes.index 'encrypted_password'] = 'password' %>
-<% end %>
+# TODO: Validations with scope ( scoped validations ) are failing
 
 require 'rails_helper'
 
-RSpec.describe <%= class_name %>, type: :model do
+RSpec.describe <%= klass %>, type: :model do
   it "should have a valid factory" do
     <%= file_name %> = FactoryGirl.build(:<%= file_name %>)
     expect(<%= file_name %>).to be_valid
   end
 
-  describe "Validations" do
-  	<% attributes.each do |attr| %>
-  		it "should ensure the presence of <%= attr %>" do
-  			<%= file_name %> = FactoryGirl.build(:<%= file_name %>, <%= attr %>: nil)
-  			expect(<%= file_name %>).not_to be_valid
-  			expect(<%= file_name %>.errors[:<%= attr %>]).to be_present
-  		end
+  describe "Validators" do
+
+  	<% if validators.include? :presence %>
+  		<% validations[:presence][0].attributes.each do |attr| %>
+  			it "should ensure the presence of <%= attr %>" do
+  				<%= file_name %> = FactoryGirl.build(:<%= file_name %>, <%= attr %>: nil)
+  				expect(<%= file_name %>).not_to be_valid
+  				expect(<%= file_name %>.errors[:<%= attr %>]).to be_present
+  			end
+  		<% end %>
   	<% end %>
 
-  	<% if attributes.include? "email" %>
-  		it "should ensure the uniqueness of email" do
-      	<%= file_name %> = FactoryGirl.create(:<%= file_name %>)
-      	new_<%= file_name %> = FactoryGirl.build(:<%= file_name %>, email: <%= file_name %>.email)
-      	expect(new_<%= file_name %>).not_to be_valid
-      	expect(new_<%= file_name %>.errors[:email]).to be_present
-    	end
+  	<% if validators.include? :uniqueness %>
+  		<% validations[:uniqueness][0].attributes.each do |attr| %>
+	  		it "should ensure the uniqueness of <%= attr %>" do
+	      	<%= file_name %> = FactoryGirl.create(:<%= file_name %>)
+	      	new_<%= file_name %> = FactoryGirl.build(:<%= file_name %>, <%= attr %>: <%= file_name %>.<%= attr %>)
+	      	expect(new_<%= file_name %>).not_to be_valid
+	      	expect(new_<%= file_name %>.errors[:<%= attr %>]).to be_present
+	    	end
+	    <% end %>
   	<% end %>
   end
 
@@ -84,7 +85,7 @@ RSpec.describe <%= class_name %>, type: :model do
 			<% supa_klasses.each do |child| %>
 				it "should destroy the associated <%= child.singularize %> when deleted" do
 					<%= file_name %> = FactoryGirl.create(:<%= file_name %>)
-					<%= file_name %>.<%= child %>.create(FactoryGirl.attributes_for(:<%= child.singularize %>))
+					<%= file_name %>.<%= child.pluralize %>.create(FactoryGirl.attributes_for(:<%= child.singularize %>))
 
 					expect{ <%= file_name %>.destroy }.to change(<%= child.camelize.singularize.constantize %>, :count).by -1
 				end
